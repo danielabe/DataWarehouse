2448fb7757bdcc28f21729561eaf78d215b08f88
Daniela <danielaberardi@live.com.ar>
2021-04-06 21:25:16 -0300

    Connection to the database in the db.js file

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 
+const { db } = require("./db")
+const { QueryTypes } = require("sequelize")
+
 app.use(express.json())
 
 app.listen(process.env.PORT || 3000, () => console.log('server started'))
+
+//users
+app.get('/users', async (req, res) => {
+    
+    const users = await db.query(`SELECT * FROM users`, { type: QueryTypes.SELECT })
+    res.status(200).json(users)
+    
+})
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -6,7 +6,36 @@ var app = express();
 
 var jwt = require('jsonwebtoken');
 
+var _require = require("./db"),
+    db = _require.db;
+
+var _require2 = require("sequelize"),
+    QueryTypes = _require2.QueryTypes;
+
 app.use(express.json());
 app.listen(process.env.PORT || 3000, function () {
   return console.log('server started');
+}); //users
+
+app.get('/users', function _callee(req, res) {
+  var users;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(db.query("SELECT * FROM users", {
+            type: QueryTypes.SELECT
+          }));
+
+        case 2:
+          users = _context.sent;
+          res.status(200).json(users);
+
+        case 4:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
 });
